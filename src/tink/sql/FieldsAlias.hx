804ae553c1384e32cdb8da7103b44f14e9346361
Ben Merckx <ben@codeurs.be>
2020-08-11 13:13:07 +0200

    Don't mutate 🤦‍♂

--- src/tink/sql/FieldsAlias.hx
+++ src/tink/sql/FieldsAlias.hx
@@ -10,8 +10,9 @@ typedef Alias<T> = {
 
 class FieldsAlias {
   public static function create<Fields>(alias:String, fields:Fields):Fields {
-    var res: DynamicAccess<ExprData<Dynamic>> = cast fields;
-    for (name => field in res) {
+    var origin: DynamicAccess<ExprData<Dynamic>> = cast fields;
+    var res: DynamicAccess<ExprData<Dynamic>> = {}
+    for (name => field in origin) {
       res[name] = switch field {
         case EField(_, name): new Field(alias, name);
         default: null;
